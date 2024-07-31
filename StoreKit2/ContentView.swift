4dbbdfccb886542403865ffe0472534308cf9968
Tim Mitra <tim@it-guy.com>
2024-07-31 11:56:46 -0400

    Initial Commit

---
+++ StoreKit2/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// --------------------------------------------
+// Original Project: StoreKit2
+// Created on 2024-07-31 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// --------------------------------------------
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoreKit2/StoreKit2App.swift
@@ -0,0 +1,21 @@
+//
+// --------------------------------------------
+// Original Project: StoreKit2
+// Created on 2024-07-31 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// --------------------------------------------
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct StoreKit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
